b1ef34e2e34da3d98cb30cfb44ccfc28f24ba6de
jow1025 <jow1025@naver.com>
2021-07-19 02:56:17 +0900

    약점체크(MST, 크루스칼,Union-find)클래스/벡터 구현

--- 1197_최소스패닝트리.cpp
+++ 1197_최소스패닝트리.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include<cstdio>
 #include <vector>
 #include <algorithm>
@@ -62,5 +62,70 @@ int main(void)
 		}
 	}
 	printf("%d\n", result);
+	return 0;
+}
+*/
+
+
+//c++버전
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+typedef struct {
+	int v1, v2, val;
+}graph;
+int v, e, res;
+vector<graph>tree;
+int parent[10001];
+bool cmp(const graph& a1, const graph& a2)
+{
+	return a1.val < a2.val;
+}
+int find_parent(int a)
+{
+	if (a == parent[a])return a;
+	//노드2가 노드1과 합집합되어있다면 노드2는 1반환(부모)
+	return parent[a] = find_parent(parent[parent[a]]);
+}
+void union_parent(int a, int b)
+{
+	//부모를 찾은 뒤 낮은 쪽으로 부모를 설정해줌(합집합)
+	a = find_parent(a);
+	b = find_parent(b);
+	//a > b ? (parent[a] = b) : (parent[b] = a);
+	if (a < b)parent[b] = a;
+	else parent[a] = b;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	graph g;
+	cin >> v >> e;
+
+	for (int i = 1; i <= v; i++)
+		parent[i] = i;
+	for (int i = 1; i <= e; i++) {
+		cin >> g.v1 >> g.v2 >> g.val;
+		tree.push_back(g);
+	}
+	
+	//간선에 대해 정렬 
+	sort(tree.begin(), tree.end(), cmp);
+
+	//주의!
+	//엣지의 갯수만큼 정점,엣지를 입력받으므로 
+	//정점 갯수만큼 탐색이 아닌 엣지갯수만큼(==tree.size())돌아야함
+	for (int i = 0; i <tree.size() ; i++) {
+		//부모를 찾아서 합집합을 찾음. 부모가 같으면 
+		if (find_parent(tree[i].v1) != find_parent(tree[i].v2))
+		{
+			res += tree[i].val;
+			//계산 후 합침
+			union_parent(tree[i].v1, tree[i].v2);
+		}
+	}
+	cout << res << endl;
 	return 0;
 }
