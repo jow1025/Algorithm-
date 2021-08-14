ba28da379848bee46eae7127ccdfbd5d5fa6a9e4
jow1025 <jow1025@naver.com>
2021-08-14 23:55:01 +0900

    dp 강훈련(8/14 주석, 다시풀기)

--- 15989_1,2,3,4더하기4.cpp
+++ 15989_1,2,3,4더하기4.cpp
@@ -3,9 +3,11 @@ using namespace std;
 #define endl '\n'
 
 int dp[10001][4];
+//2차원 배열로 선언
 
 int solve(int n, int k)
 {
+	//종료문
 	if (dp[n][k])return dp[n][k];
 	for (int i = 4; i <= n; i++)
 	{
@@ -33,8 +35,9 @@ int main()
 	cin.tie(0);
 	int T;
 	cin >> T;
+	//1,2,3행 각각 1, (1,2), (1,2,3)을 1로 처리해야함
 	dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
-	for (; T--;)
+	while(T--)
 	{
 		int input;
 		cin >> input;
