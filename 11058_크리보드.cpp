653733ae6cf2c4307d2a3e8bfd5f4662e6dfd9b8
jow1025 <jow1025@naver.com>
2021-08-15 23:47:09 +0900

    dp 강훈련(8/15)

---
+++ 11058_크리보드.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include <algorithm>
+
+#include <cstring>
+
+using namespace std;
+
+const int MAX = 100 + 1;
+int N;
+
+long long cache[MAX];
+long long func(int idx)
+{
+	long long& result = cache[idx];
+	if (result != -1)
+
+		return result;
+
+	result = 1 + func(idx - 1); 
+
+	if (idx >= 3)
+
+		for (int i = 1; i <= idx - 2; i++)
+
+			result = max(result, func((idx - 2) - i) * (i + 1));
+
+	return result;
+
+}
+
+int main(void)
+{
+
+	ios_base::sync_with_stdio(0);
+
+	cin.tie(0);
+	cin >> N;
+
+	
+	memset(cache, -1, sizeof(cache));
+	cache[0] = 0;
+	cout << func(N) << "\n";
+	return 0;
+
+}
